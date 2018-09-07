fbf042ba8bca666e73a04d9c0cbeab3740fac1e2
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-09-06 20:04:30 -0500

    add delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,18 +10,26 @@ class PostsController < ApplicationController
 
   def new
     # Return view to create a new Post
+    @post = Post.new
   end
 
   def create
     # Add a new `Post` to the database
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def destroy
     # Remove a `Post` from the database
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
