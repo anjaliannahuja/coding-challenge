fad7e41cbf3079c4ec1aceeb78c50995f08a8963
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-09-06 19:50:37 -0500

    ability to add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     # Return all `Post`
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -9,9 +14,18 @@ class PostsController < ApplicationController
 
   def create
     # Add a new `Post` to the database
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
   end
 
   def destroy
     # Remove a `Post` from the database
   end
+
+  private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
