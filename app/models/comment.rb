0c5d2a4e73a6e6431d4e6bba4ad1aad6c0e3f2c9
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-09-09 21:05:26 -0500

    added comments to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  # Comment is child of post
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+  # Can have many comments, delete all comments if post deleted
   has_many :comments, dependent: :destroy
 
+  # Add in search capablities to search both title and body fields
   def self.search(term)
     if term
       where('title LIKE ? OR body LIKE ?', "%#{term}%", "%#{term}%")
